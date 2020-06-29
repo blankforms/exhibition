909d4384352bc0e812ec95884bf4fdb8bfded9ca
blankforms <blankformsdeveloper@gmail.com>
2020-06-29 15:42:29 -0700

    attempting newline chars in email necode

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//version 1.0.9.7
+//version 1.0.9.9.1
 
 //iOS resizing issue from portrait to landscape
 //https://stackoverflow.com/questions/5434656/ipad-layout-scales-up-when-rotating-from-portrait-to-landscape
@@ -181,9 +181,9 @@ function sendMail() {
              + "?cc="
              + "&subject=" + escape("Inquiry Request for ")
              + escape(document.getElementById('form-details').textContent.slice(0, 20))
-             + "&body=" + escape(document.getElementById('inquiry-mail').value) +escape(
-             )
-             + escape(document.getElementById('return-address').value)
+             + "&body=" + escape(document.getElementById('inquiry-mail').textContent)
+             +"%0A%0A"
+             + escape(document.getElementById('return-address').textContent)
     ;
 
     window.location.href = link;
