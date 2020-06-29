07c18f029150b92d45a12313f6100d939b907419
blankforms <blankformsdeveloper@gmail.com>
2020-06-29 15:52:07 -0700

    small email encode revision

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//version 1.0.9.9.3
+//version 1.0.9.9.4
 
 //iOS resizing issue from portrait to landscape
 //https://stackoverflow.com/questions/5434656/ipad-layout-scales-up-when-rotating-from-portrait-to-landscape
@@ -181,7 +181,7 @@ function sendMail() {
              + "?cc="
              + "&subject=" + escape("Inquiry Request for ")
              + escape(document.getElementById('form-details').textContent.slice(0, 20))
-             + "&body=" + escape(document.getElementById('inquiry-mail').textContent) + escape("\n") + escape(document.getElementById('return-address').value)
+             + "&body=" + escape(document.getElementById('inquiry-mail').value) + escape("\n") + escape(document.getElementById('return-address').value)
     ;
 
     window.location.href = link;
