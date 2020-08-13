6e9041a34b22a1cb9265bf9d6d8e8fab6c5c6124
blankforms <blankformsdeveloper@gmail.com>
2020-08-12 18:48:57 -0700

    removed Graham Lambkin from js.main inquiry form subject line

--- when-sun-comes-out/js/main.js
+++ when-sun-comes-out/js/main.js
@@ -32,7 +32,7 @@ function closeInquiryForm() {
 function sendMail() {
     var link = "mailto:julia@blankforms.org"
              + "?cc="
-             + "&subject=" + escape("Inquiry Request for Graham Lambkin, ")
+             + "&subject=" + escape("Inquiry Request for ")
              + escape(document.getElementById('form-details').textContent.slice(0, 20))
              + "&body=" + escape(document.getElementById('inquiry-mail').value) + escape('\n\nreturn address: ') + escape(document.getElementById('return-address').value)
     ;
